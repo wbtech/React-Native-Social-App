de94726d8466a74f330bb7c67a7879cad4c52651
Wale Osho <90667947+wbtech@users.noreply.github.com>
2023-01-26 02:56:05 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, FlatList, Button } from "react-native";
+
+
+export default function App() {
+ 
+
+ 
+
+  return (
+    <>
+      <StatusBar style='light' />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
